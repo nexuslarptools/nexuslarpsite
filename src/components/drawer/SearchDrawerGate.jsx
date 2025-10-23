eab7bf8c4a1e991aa75ec2ef848c0b2e590ef5bb
Nick Burtner <cstrife@gmail.com>
2025-10-23 11:50:17 -0400

    Switching to middleware

---
+++ src/components/drawer/SearchDrawerGate.jsx
@@ -0,0 +1,23 @@
+﻿import PropTypes from 'prop-types'
+import AuthLevelInfo from '../../utils/authLevelInfo'
+import SearchDrawer from './searchdrawer'
+
+// Renders the SearchDrawer only when the user is authenticated
+const SearchDrawerGate = (props) => {
+  const authLevel = AuthLevelInfo();
+  const isAuthenticated = authLevel > 0;
+  const isLoading = authLevel === 0;
+
+  if (isLoading || !isAuthenticated) return null;
+
+  return (
+    <SearchDrawer open={props.open} toggleClose={props.toggleClose} />
+  );
+}
+
+SearchDrawerGate.propTypes = {
+  open: PropTypes.bool,
+  toggleClose: PropTypes.func,
+};
+
+export default SearchDrawerGate
